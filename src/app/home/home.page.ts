39828a2dcb2be464520556bcad3928ee90a449ff
Rick <real4ric@gmail.com>
2021-04-23 18:13:33 +0600

    Final commit before change

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,22 +10,25 @@ export class HomePage {
   public timeLeftInSeconds = 0;                     //Time Left
   private durationInSeconds = 4 * 60;               //Duration of cooking
 
-  private timer = null;                             // This is the timer (chronometre)
+  public timer;                             // This is the timer (chronometre)
 
 
   constructor() {}
 
     public startTimer () {
+      if(this.timeLeftInSeconds == 0) {
       this.timeLeftInSeconds = this.durationInSeconds;  //Time left is = total time
-      this.timer = setInterval(                         // Decrement every second
-        () => {
-          this.timeLeftInSeconds--;
-          if (this.timeLeftInSeconds == 0){
-            clearInterval(this.timer);
-          }
-        }, 1000
-      );
     }
+        this.timer = setInterval(                         // Decrement every second
+            () => {
+              this.timeLeftInSeconds--;
+              if (this.timeLeftInSeconds == 0){
+                clearInterval(this.timer);
+                this.timer=null;
+              }
+            }, 1000
+          );
+        }
 
 
     public displayTime(timeInSeconds) {
@@ -33,6 +36,17 @@ export class HomePage {
       let seconds = timeInSeconds % 60; 
 
       return minutes + ' : ' + seconds;
+    }
+
+    public stopTimer() {
+      clearInterval(this.timer);
+      this.timeLeftInSeconds = 0;
+      this.timer=null;
+    }
+
+    public pauseTimer(){
+      clearInterval(this.timer);
+      this.timer=null;
     }
 
 }
